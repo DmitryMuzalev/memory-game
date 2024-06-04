78b0eae0cfe34b8cafb8f44a9d8667a220462c74
DmitryMuzalev <56193550+DmitryMuzalev@users.noreply.github.com>
2024-06-04 16:43:45 +0300

    feat: Add icons.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+import data from "./data";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      {data.map((item, i) => {
+        return <img key={i} src={item.icon} alt="test" />;
+      })}
+    </div>
+  );
 }
 
 export default App;
---
+++ src/data/index.js
@@ -0,0 +1,6 @@
+const cards = [
+  { number: "1", icon: "../assets/images/icons/bug.svg" },
+  { number: "2", icon: "../assets/images/icons/camera.svg" },
+];
+
+export default cards;

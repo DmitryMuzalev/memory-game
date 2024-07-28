b158a80a36dab864790bc3dc5fae51a6006192b7
DmitryMuzalev <56193550+DmitryMuzalev@users.noreply.github.com>
2024-07-28 19:59:08 +0300

    refactor: Refactoring the Button component.

--- src/components/UI/Button/Button.jsx
+++ src/components/UI/Button/Button.jsx
@@ -1,18 +1,17 @@
-import { clsx } from "clsx";
-import styles from "./Button.module.scss";
+import { clsx } from 'clsx';
+import styles from './Button.module.scss';
 
 function Button({
   children,
-  type = "default",
+  type = 'default',
   isCircle = false,
   isActive = false,
   cb = () => {},
 }) {
   const stylesForButton = clsx(
     styles.button,
-    type === "primary" && styles.buttonPrimary,
-    type === "secondary" && styles.buttonSecondary,
-    type === "default" && styles.buttonDefault,
+    type === 'primary' && styles.buttonPrimary,
+    type === 'secondary' && styles.buttonSecondary,
     isCircle && styles.buttonCircle,
     isActive && styles.buttonActive
   );

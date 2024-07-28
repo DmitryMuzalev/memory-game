0052d4693f94a0e78238831fb87ccaa7fa0fbd11
DmitryMuzalev <56193550+DmitryMuzalev@users.noreply.github.com>
2024-07-28 22:13:50 +0300

    refactor: Card component changed.

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,18 +1,22 @@
-import styles from "./Card.module.scss";
-import clsx from "clsx";
-import { useSelector } from "react-redux";
-import { getSettings } from "../../features/Settings/settings-slice";
+import styles from './Card.module.scss';
+import clsx from 'clsx';
+import { useSelector } from 'react-redux';
+import { getSettings } from '../../features/Settings/settings-slice';
 
 function Card({ number, icon, status, cb }) {
   const { theme } = useSelector(getSettings);
-  const stylesForCard = clsx(styles.card, status === "open" && styles.cardOpen);
+  const stylesForCard = clsx(styles.card, status === 'open' && styles.cardOpen);
 
   return (
-    <button className={stylesForCard} onClick={cb}>
+    <button
+      className={stylesForCard}
+      onClick={cb}
+      disabled={status === 'guessed'}
+    >
       <div className={styles.cardInner}>
         <div className={styles.cardFront}></div>
         <div className={styles.cardBack}>
-          {theme === "numbers" ? number : icon}
+          {theme === 'numbers' ? number : icon}
         </div>
       </div>
     </button>

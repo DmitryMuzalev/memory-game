0968ec0552f0451edaa95c2d3c8e0d93dddcc1c5
DmitryMuzalev <56193550+DmitryMuzalev@users.noreply.github.com>
2024-09-01 16:07:24 +0300

    fix: Enable strict mode.

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
-//import React from "react"; // for  <React.StrictMode>
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.scss";
-import { Provider } from "react-redux";
-import { store } from "./store";
+import React from 'react'; // for  <React.StrictMode>
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.scss';
+import { Provider } from 'react-redux';
+import { store } from './store';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
 );
